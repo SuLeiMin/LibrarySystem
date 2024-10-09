e247df0cf731ace6bd56760b8b116e3f8c8e9475
suleimin <suleimin@gmail.com>
2024-10-09 23:34:26 +0900

    update_index.html
    added search button and welcome page

--- src/main/java/com/slm/librarysystem/controller/IndexController.java
+++ src/main/java/com/slm/librarysystem/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.slm.librarysystem.model.Info;
 
@@ -14,15 +15,24 @@ import com.slm.librarysystem.model.Info;
 public class IndexController {
 
 	@GetMapping("/")
-	public String indexPage(Model model) {
-		List<String> list = new ArrayList<>();
-		list.add("中央図書館 ガラスケース展示「夢みる女性誌展（6～7月）」");
-		list.add("推し活企画】「店主の本棚」6月は、カレー屋の「インディアンラソイ」さん");
-		list.add("文庫本ぷちリサイクル」を実施します");
-		model.addAttribute("list",list);
+	public String indexPage() {
+//		List<String> list = new ArrayList<>();
+//		list.add("中央図書館 ガラスケース展示「夢みる女性誌展（6～7月）」");
+//		list.add("推し活企画】「店主の本棚」6月は、カレー屋の「インディアンラソイ」さん");
+//		list.add("文庫本ぷちリサイクル」を実施します");
+//		model.addAttribute("list",list);
 		return "index";
 	}
-
+	@GetMapping("/search")
+    public String searchBooks(@RequestParam(value = "query", required = false) String query, Model model) {
+        if (query != null && !query.isEmpty()) {
+            // Perform search using the query
+            //List<Book> searchResults = bookService.searchBooks(query);
+            // Add the search results to the model
+            //model.addAttribute("searchResults", searchResults);
+        }
+        return "index"; // Return the same template (e.g., library.html)
+    }
 	
 	@GetMapping("/showCategory")
 	public String showCategory() {
